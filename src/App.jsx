7d01912629bda4b66817e9992a9b7f583398a3a3
Muhammad Ammad <154667373+Muhammad-Ammad3@users.noreply.github.com>
2026-05-31 22:30:11 +0500

    fix about import

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ import Agreement from "./pages/Agreement";
 import Apply from "./pages/Apply";
 import Community from "./pages/Community";
 import Digital from "./pages/Digital";
-import About from "./pages/about";
+import About from "./pages/About";
 import InternshipDetail from "./components/InternshipDetail";
 import UserDashboard from "./pages/UserDashboard";
 
